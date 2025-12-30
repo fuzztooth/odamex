2400250df43a115e5a2736e0f829a744506ac69c
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-12-29 19:12:03 -0600

    Add some missing cases to DFloor constructor

--- server/src/sv_main.cpp
+++ server/src/sv_main.cpp
@@ -4219,7 +4219,7 @@ BEGIN_COMMAND (playerinfo)
 	}
 	else
 	{
-		PrintFmt(" frags - {:d}  deaths - {:d}  points - %d\n", player->fragcount,
+		PrintFmt(" frags - {:d}  deaths - {:d}  points - {:d}\n", player->fragcount,
 		       player->deathcount, player->points);
 	}
 	if (g_lives)
